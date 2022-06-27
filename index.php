b8c8e2a17e8b919ac834f8e80abd9e0c46362694
Carlinbarograund <89025004+Carlinbarograund@users.noreply.github.com>
2022-06-27 09:32:39 -0300

    eu nem sei

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fodasse.png</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        $nome1="John Cenaaaaaaa";
+        $numero=10;
+    echo "This name is $nome1 tururururuuuuuuu<br>";
+    print "O cachorro do mato nunca para, ele só da um tempo<br>";
+
+    var_dump($nome1);
+    echo "<br>";
+    var_dump($numero);
+
+    ?>
+</body>
+</html>
